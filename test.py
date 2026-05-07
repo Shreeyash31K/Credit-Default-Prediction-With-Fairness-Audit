44cc49e232f1f760e93dfa11b801e0bb094d2ce8
Shreeyash31K <shreeyash.kole1717@gmail.com>
2026-05-07 18:24:45 +0530

    Initial project setup

---
+++ test.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import sklearn
+import xgboost
+import lightgbm
+import shap
+import fairlearn
+import fastapi
+import streamlit
+
+print("All libraries installed successfully!")
